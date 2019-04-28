b1e492e79798a43b072beb7b04240e30cb8d7f4a
Zachary Uht <zachuht@gmail.com>
2019-04-28 17:49:14 +0000

    add mongoose server

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://zachuht:Hyfxqz!1@cluster0-faxtu.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
