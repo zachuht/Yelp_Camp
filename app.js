e6379c2e1fffa5ed8b5764c53bc507d89bd7e34d
Zachary Uht <zachuht@gmail.com>
2019-04-28 20:57:58 +0000

    update DB url V2

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-//mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-mongoose.connect("mongodb+srv://zachuht:Hyfxqz!1@cluster0-faxtu.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+//mongoose.connect("mongodb+srv://zachuht:Hyfxqz!1@cluster0-faxtu.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
