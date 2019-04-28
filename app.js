964a2821554f7de524b8a5f4a94199b0b8bd5674
Zachary Uht <zachuht@gmail.com>
2019-04-28 21:03:27 +0000

    remove sensitive info

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
+//var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp"
+//mongoose.connect(url);
+
 mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-//mongoose.connect("mongodb+srv://zachuht:Hyfxqz!1@cluster0-faxtu.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
+//mongoose.connect("mongodb+srv://<username>:<password>@cluster0-faxtu.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
